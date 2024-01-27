1b930cbc46ef3cfc6e53b40eca4b973653a3b813
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2024-01-27 16:33:30 +0300

    feat: implement isPalindrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -215,8 +215,12 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let strToCompare = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    strToCompare += str[i];
+  }
+  return str === strToCompare;
 }
 
 /**
